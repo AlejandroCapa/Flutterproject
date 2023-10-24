6eaa433def78c0c8e814b33de45cb125ab1a45c4
John Solano <john.solano@tutenlabs.com>
2023-10-24 10:39:04 -0500

    Ajustes

--- lib/src/login_page.dart
+++ lib/src/login_page.dart
@@ -14,17 +14,19 @@ class _LoginpageState extends State<Loginpage> {
       child: Scaffold(
         body: Center(
           child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Añadido para centrar en vertical
             children: [
               Image.asset(
-                "images/logo.png",
+                "/images/login.png",
                 height: 300.0,
               ),
-              SizedBox(height: 15.0,),
-              _userTextfield(),
-              SizedBox(height: 15,),
-              _passwordTextfield(),
-              SizedBox(height: 20.0,),
-              _bottonlogin(),
+              SizedBox(height: 15.0),
+              _userTextField(), // Corregido el nombre de la función
+              SizedBox(height: 15),
+              _passwordTextField(), // Corregido el nombre de la función
+              SizedBox(height: 20.0),
+              _buttonLogin(), // Corregido el nombre de la función
             ],
           ),
         ),
@@ -32,61 +34,53 @@ class _LoginpageState extends State<Loginpage> {
     );
   }
 
-  Widget _userTextfield() {
-    return StreamBuilder(
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-      return Container(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-             icon: Icon(Icons.email),
-              hintText: "example@correo.com",
-              labelText: "Correo electronico",
-              ),
-          onChanged: (value) {
-            
-          },
+  Widget _userTextField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.email),
+          hintText: "example@correo.com",
+          labelText: "Correo electrónico", // Corregido el texto
         ),
-      );
-    });
-  }
-
-  Widget _passwordTextfield() {
-    return StreamBuilder(
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        return Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              icon: Icon(Icons.lock),
-              hintText: "Contrasñea",
-              labelText: "Contraseña",
-            ),
-          ),
-        );
-      },
+        onChanged: (value) {},
+      ),
     );
   }
 
-  Widget _bottonlogin() {
-    return StreamBuilder(
-      builder:(BuildContext (context, AsyncSnapshot snapshot) {
-        return RaisedButton(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 80.0,vertical: 15.0),
-            child: Text("Iniciar Sesion"),
-              ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              ),
-              elevation: 10.0,
-              color: Colors.amberAccent,
-              onPressed: (){}
-           );
-          }
-        ), 
-      );
+  Widget _passwordTextField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType
+            .text, // Cambiado a TextInputType.text para contraseñas
+        obscureText: true, // Para ocultar la contraseña
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock),
+          hintText: "Contraseña",
+          labelText: "Contraseña",
+        ),
+        onChanged: (value) {},
+      ),
+    );
   }
-} 
+
+  Widget _buttonLogin() {
+    return ElevatedButton(
+      //Se cambio el metodo anterior que es de versiones antiguas de flutter
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        primary: Colors.amberAccent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 10.0,
+      ),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: Text("Iniciar Sesión"),
+      ),
+    );
+  }
+}
